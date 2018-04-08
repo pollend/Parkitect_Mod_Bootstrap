8c0d196badb1858e5d6598ff6fbe2b19671c08b9
Michael Pollind <mpollind@gmail.com>
2018-04-08 14:20:24 -0700

    removed flatride animator in favor of unity animations

--- Model/ParkitectObjects/FlatRideParkitectObject.cs
+++ Model/ParkitectObjects/FlatRideParkitectObject.cs
@@ -28,12 +28,7 @@ public class FlatRideParkitectObject : ParkitectObj
 		"Transport Rides",
 		"Water Rides"
 	};
-	
-	private int _phaseNum;
 
-	[SerializeField] public List<Motor> Motors = new List<Motor>();
-	[SerializeField] public List<Phase> Phases = new List<Phase>();
-	[SerializeField] public Phase CurrentPhase;
 	[SerializeField] public bool Animating;
 	[SerializeField] public float Excitement;
 	[SerializeField] public float Intensity;
@@ -58,175 +53,7 @@ public class FlatRideParkitectObject : ParkitectObj
 
 
 
-
-	public void Animate(Transform root)
-	{
-		Motors.RemoveAll(x => x == null);
-		Phases.RemoveAll(x => x == null);
-
-		foreach (Motor m in Motors)
-		{
-			m.Enter(root);
-		}
-
-		if (Phases.Count <= 0)
-		{
-			Animating = false;
-			foreach (Motor m in Motors)
-			{
-				m.Reset(root);
-			}
-
-			foreach (MultipleRotations R in Motors.OfType<MultipleRotations>())
-			{
-				R.Reset(root);
-			}
-
-			return;
-		}
-
-		foreach (Motor m in Motors)
-		{
-			m.Enter(root);
-		}
-
-		Animating = true;
-		_phaseNum = 0;
-		CurrentPhase = Phases[_phaseNum];
-		CurrentPhase.Running = true;
-		CurrentPhase.Enter();
-		CurrentPhase.Run(root);
-	}
-
-	void NextPhase(Transform root)
-	{
-
-		CurrentPhase.Exit();
-		CurrentPhase.Running = false;
-		_phaseNum++;
-		if (Phases.Count > _phaseNum)
-		{
-			CurrentPhase = Phases[_phaseNum];
-			CurrentPhase.Running = true;
-			CurrentPhase.Enter();
-			CurrentPhase.Run(root);
-			return;
-		}
-
-		Animating = false;
-		foreach (Rotator m in Motors.OfType<Rotator>())
-		{
-			m.Enter(root);
-
-		}
-
-		foreach (Rotator m in Motors.OfType<Rotator>())
-		{
-			Transform transform = m.Axis.FindSceneRefrence(root);
-			if (transform != null)
-				transform.localRotation = m.OriginalRotationValue;
-
-		}
-
-		foreach (RotateBetween m in Motors.OfType<RotateBetween>())
-		{
-			Transform transform = m.Axis.FindSceneRefrence(root);
-			if (transform != null)
-				transform.localRotation = m.OriginalRotationValue;
-
-		}
-
-		foreach (Mover m in Motors.OfType<Mover>())
-		{
-			Transform transform = m.Axis.FindSceneRefrence(root);
-			if (transform != null)
-				transform.localPosition = m.OriginalRotationValue;
-
-		}
-
-		CurrentPhase = null;
-	}
-
-
-
-	public void Run(Transform transform)
-	{
-		if (CurrentPhase != null)
-		{
-			CurrentPhase.Run(transform);
-			if (!CurrentPhase.Running)
-			{
-				NextPhase(transform);
-			}
-		}
-	}
 #if UNITY_EDITOR
-	public void AddMotor(Motor motor)
-	{
-		AssetDatabase.AddObjectToAsset(motor, this);
-		EditorUtility.SetDirty(this);
-		AssetDatabase.SaveAssets();
-
-		Motors.Add(motor);
-	}
-
-	public void RemoveMotor(Motor motor)
-	{
-		Motors.Remove(motor);
-		DestroyImmediate(motor, true);
-	}
-
-	public void AddPhase(Phase phase)
-	{
-		AssetDatabase.AddObjectToAsset(phase, this);
-		EditorUtility.SetDirty(this);
-		AssetDatabase.SaveAssets();
-
-		Phases.Add(phase);
-	}
-
-	public void RemovePhase(Phase phase)
-	{
-		Phases.Remove(phase);
-		DestroyImmediate(phase, true);
-	}
-
-	public override void PrepareForExport()
-	{
-		for (int x = 0; x < Motors.Count; x++)
-		{
-			Motors[x].PrepareExport(this);
-		}
-
-		base.PrepareForExport();
-	}
-
-	public override void CleanUp()
-	{
-		for (int x = 0; x < Phases.Count; x++)
-		{
-			if (Phases[x] != null)
-			{
-				Phases[x].CleanUp();
-				DestroyImmediate(Phases[x], true);
-			}
-		}
-
-		for (int x = 0; x < Motors.Count; x++)
-		{
-			if (Motors[x] != null)
-			{
-				DestroyImmediate(Motors[x], true);
-			}
-		}
-
-		Motors.Clear();
-		Phases.Clear();
-
-
-		base.CleanUp();
-	}
-
 
 	public override void RenderInspectorGui()
 	{
@@ -246,32 +73,34 @@ public class FlatRideParkitectObject : ParkitectObj
 		XSize = EditorGUILayout.IntField("X", XSize);
 		ZSize = EditorGUILayout.IntField("Z", ZSize);
 
-		FlatRideCategory = ATTRACTION_CATEGORY_TAGS[EditorGUILayout.Popup ("category", Array.IndexOf(ATTRACTION_CATEGORY_TAGS,FlatRideCategory), ATTRACTION_CATEGORY_INDEX)]; 
+		FlatRideCategory = ATTRACTION_CATEGORY_TAGS[EditorGUILayout.Popup ("category", Array.IndexOf(ATTRACTION_CATEGORY_TAGS,FlatRideCategory), ATTRACTION_CATEGORY_INDEX)];
 
-		
-		if (GUILayout.Button("Generate outer grid"))
+		WaypointDecorator waypointDecorator = (WaypointDecorator) GetDecorator(typeof(WaypointDecorator), false);
+		if (waypointDecorator != null && waypointDecorator.EnableEditing)
 		{
-
-			float minX = -XSize / 2;
-			float maxX = XSize / 2;
-			float minZ = -ZSize / 2;
-			float maxZ = ZSize / 2;
-			for (int xi = 0; xi < Mathf.RoundToInt(maxX - minX); xi++)
+			if (GUILayout.Button("Generate outer grid"))
 			{
-				for (int zi = 0; zi < Mathf.RoundToInt(maxZ - minZ); zi++)
-				{
-					float x = minX + xi;
-					float z = minZ + zi;
-					if (!(x == minX || x == maxX - 1) && !(z == minZ || z == maxZ - 1))
-					{
-						continue;
-					}
 
-					WaypointDecorator waypointDecorator = (WaypointDecorator) GetDecorator(typeof(WaypointDecorator), false);
-					SPWaypoint newWaypoint = new SPWaypoint();
-					newWaypoint.localPosition = new Vector3(x + 0.5f, waypointDecorator.HelperPlaneY, z + 0.5f);
-					newWaypoint.isOuter = true;
-					waypointDecorator.Waypoints.Add(newWaypoint);
+				float minX = -XSize / 2;
+				float maxX = XSize / 2;
+				float minZ = -ZSize / 2;
+				float maxZ = ZSize / 2;
+				for (int xi = 0; xi < Mathf.RoundToInt(maxX - minX); xi++)
+				{
+					for (int zi = 0; zi < Mathf.RoundToInt(maxZ - minZ); zi++)
+					{
+						float x = minX + xi;
+						float z = minZ + zi;
+						if (!(x == minX || x == maxX - 1) && !(z == minZ || z == maxZ - 1))
+						{
+							continue;
+						}
+
+						SPWaypoint newWaypoint = new SPWaypoint();
+						newWaypoint.localPosition = new Vector3(x + 0.5f, waypointDecorator.HelperPlaneY, z + 0.5f);
+						newWaypoint.isOuter = true;
+						waypointDecorator.Waypoints.Add(newWaypoint);
+					}
 				}
 			}
 		}
@@ -281,6 +110,7 @@ public class FlatRideParkitectObject : ParkitectObj
 	public override void RenderSceneGui()
 	{
 		GameObject refrence = GetGameObjectRef(false);
+		
 		if (refrence == null)
 			return;
 
@@ -375,22 +205,6 @@ public class FlatRideParkitectObject : ParkitectObj
 
 	public override void DeSerialize(Dictionary<string, object> elements)
 	{
-		foreach (var ele in (List<object>) elements["Motors"])
-		{
-			Dictionary<string, object> serializedMotor = ele as Dictionary<string, object>;
-			Motor motor = (Motor) CreateInstance(Motor.FindMotorTypeByTag((string) serializedMotor["@Tag"]));
-			motor.Deserialize(serializedMotor);
-			Motors.Add(motor);
-		}
-
-		foreach (var ele in (List<object>) elements["Phases"])
-		{
-			Phase phase = CreateInstance<Phase>();
-			phase.Deserialize(ele as Dictionary<string, object>, Motors.ToArray());
-			Phases.Add(phase);
-		}
-
-
 		if (elements.ContainsKey("Excitement"))
 			Excitement = Convert.ToSingle(elements["Excitement"]);
 		if (elements.ContainsKey("Intensity"))
@@ -411,21 +225,6 @@ public class FlatRideParkitectObject : ParkitectObj
 	{
 		Dictionary<string, object> result = base.Serialize();
 
-		List<Dictionary<string, object>> serializedMotors = new List<Dictionary<string, object>>();
-		foreach (var motor in Motors)
-		{
-			Dictionary<string, object> serializedMotor = motor.Serialize(Prefab.transform);
-			serializedMotor.Add("@Tag", Motor.GetTagFromMotor(motor.GetType()));
-			serializedMotors.Add(serializedMotor);
-		}
-
-		List<Dictionary<string, object>> serializedPhases = new List<Dictionary<string, object>>();
-		foreach (var phase in Phases)
-		{
-			serializedPhases.Add(phase.Serialize(Prefab.transform, Motors.ToArray()));
-		}
-
-
 		result.Add("Excitement", Excitement);
 		result.Add("Intensity", Intensity);
 		result.Add("Nausea", Nausea);
@@ -433,8 +232,6 @@ public class FlatRideParkitectObject : ParkitectObj
 		result.Add("ZSize", ZSize);
 		result.Add("Category", FlatRideCategory);
 
-		result.Add("Phases", serializedPhases);
-		result.Add("Motors", serializedMotors);
 		return result;
 	}
 
